97a0f0e767327148e1cdd955e676da606de2d37f
Erick99Noleto <noletoerick1@gmail.com>
2023-09-26 22:01:50 -0300

    ffffffffffffff

--- questao_05.c
+++ questao_05.c
@@ -2,28 +2,43 @@
 #include<stdlib.h>
 int main(){
 
-    int a, b, maior;
+    int a, b;
 
     scanf("%d %d", &a, &b);
 
-    if(a>b){
-        maior = a;
+    if(a==b){
+        printf("Os valores lidos sao iguais\n");
+    } else if(a!=b && a == 0){
+        printf("%d eh maior que %d\n", b, a);
+        printf("%d nao eh multiplo de %d\n", b, a);
+
+    } else if(a!=b && b == 0){
         printf("%d eh maior que %d\n", a, b);
-            if(maior%b==0){
+        printf("%d nao eh multiplo de %d\n", a, b);
+
+    } else if(a > b){
+        
+        printf("%d eh maior que %d\n", a, b);
+            if(a%b==0){
                 printf("%d eh multiplo de %d\n", a, b);
             }else{
                 printf("%d nao eh multiplo de %d\n", a, b);
             }
-    }else if(a<b){
-        maior = b;
+    } else if( a < b){
         printf("%d eh maior que %d\n", b, a);
-            if(maior%a==0){
+            if(b%a==0){
                 printf("%d eh multiplo de %d\n", b, a);
             }else{
                 printf("%d nao eh multiplo de %d\n", b, a);
             }
-    }else if(a==b){
-        printf("Os valores lidos sao iguais\n");
     }
+
+
+
+
+
+
+
+
     return 0;
 }
